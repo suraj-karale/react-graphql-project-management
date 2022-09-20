172baca3be4d4b357b85cc31b66e5f1fa1cfad76
sbkcodes <88947267+sbkcodes@users.noreply.github.com>
2022-09-20 19:43:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,10 +22,14 @@ app.use(
   })
 );
 
-app.use(express.static(path.join(__dirname, "client", "build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+}
 
 app.listen(port, console.log(`Server running on port ${port}`));
