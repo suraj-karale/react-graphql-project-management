dee57bae87b8ef601363a656f35cc6144f78ed5a
sbkcodes <88947267+sbkcodes@users.noreply.github.com>
2022-09-21 15:08:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,22 +14,26 @@ connectDB();
 
 app.use(cors());
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    graphiql: process.env.NODE_ENV === "development",
-  })
-);
+__dirname = path.resolve();
 
 if (
   process.env.NODE_ENV === "production" ||
   process.env.NODE_ENV === "staging"
 ) {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    res.sendFile(
+      path.resolve(path.join(__dirname, "client", "build", "index.html"))
+    );
   });
+} else {
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema,
+      graphiql: process.env.NODE_ENV === "development",
+    })
+  );
 }
 
 app.listen(port, console.log(`Server running on port ${port}`));
